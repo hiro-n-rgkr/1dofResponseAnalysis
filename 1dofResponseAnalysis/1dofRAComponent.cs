b93d5a62e3355f43c5db02bc700355c81f6f5189
hiron <hiro.n.rgrk@gmail.com>
2018-08-19 11:49:56 +0900

    [update] rhino上への串団子出力コンポーネント作成

--- 1dofResponseAnalysis/1dofRAComponent.cs
+++ 1dofResponseAnalysis/1dofRAComponent.cs
@@ -5,19 +5,11 @@ See LICENSE
 **/
 
 using System;
-using System.Drawing;
 using System.Collections.Generic;
 
 using Grasshopper.Kernel;
-using Grasshopper.Kernel.Types;
-using Grasshopper.Kernel.Attributes;
-using Grasshopper.GUI;
-using Grasshopper.GUI.HTML;
-using Grasshopper.GUI.Canvas;
-using Grasshopper.GUI.Gradient;
 
-using GH_IO.Serialization;
-
+using Rhino;
 using Rhino.Geometry;
 
 
@@ -47,7 +39,7 @@ namespace GH_NewmarkBeta
             pManager.AddNumberParameter("Stiffness", "K", "Spring Stiffness(kN/m)", GH_ParamAccess.item);
             pManager.AddNumberParameter("Damping ratio", "h", "Damping ratio", GH_ParamAccess.item);
             pManager.AddNumberParameter("Time Increment", "dt", "Time Increment(sec)", GH_ParamAccess.item);
-            pManager.AddNumberParameter("Beta", "Beta", "Parameters of Newmark β ", GH_ParamAccess.item);
+            pManager.AddNumberParameter("Beta", "Beta", "Parameters of Newmark β ", GH_ParamAccess.item, 0.25);
             pManager.AddIntegerParameter("N", "N", "Parameters of Newmark β ", GH_ParamAccess.item);
             pManager.AddTextParameter("WAVE", "WAVE", "Parameters of Newmark β ", GH_ParamAccess.item);
         }
@@ -57,14 +49,19 @@ namespace GH_NewmarkBeta
         /// </summary>
         protected override void RegisterOutputParams(GH_OutputParamManager pManager)
         {
-            pManager.AddNumberParameter("Model", "Model", "output Model Infomation", GH_ParamAccess.item);
+            pManager.AddNumberParameter("Model", "Model", "output Model Data", GH_ParamAccess.item);
             pManager.AddNumberParameter("Acceleration", "Acc", "output Acceleration(cm/s^2)", GH_ParamAccess.item);
             pManager.AddNumberParameter("Velocity", "Vel", "output Velocity(cm/s)", GH_ParamAccess.item);
             pManager.AddNumberParameter("Displacement", "Disp", "output Displacement(cm)", GH_ParamAccess.item);
         }
+
+        /// <summary>
+        /// 解析を実行する箇所
+        /// </summary>
         protected override void SolveInstance(IGH_DataAccess DA)
         {
             // パラメータの定義 ＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝
+            List<double> Model = new List<double>();
             double M = double.NaN;    // 質量
             double K = double.NaN;    // 剛性
             double h = double.NaN;    // 減衰定数
@@ -81,6 +78,10 @@ namespace GH_NewmarkBeta
             if (!DA.GetData(4, ref beta)) { return; }
             if (!DA.GetData(5, ref N)) { return; }
             if (!DA.GetData(6, ref wave_str)) { return; }
+
+            // モデルデータ出力用＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝
+            Model.Add(M);
+            Model.Add(K);
 
             //　地震波データの処理　＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝
             //　カンマ区切りで波形を入力するので、カンマで区切り配列に入れている
@@ -102,9 +103,10 @@ namespace GH_NewmarkBeta
             slv.NewmarkBeta(M, K, h, dt, beta, N, wave, ref out_a, ref out_v, ref out_d);
 
             // grassshopper へのデータ出力　＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝
-            DA.SetDataList(0, out_a);
-            DA.SetDataList(1, out_v);
-            DA.SetDataList(2, out_d);
+            DA.SetDataList(0, Model);
+            DA.SetDataList(1, out_a);
+            DA.SetDataList(2, out_v);
+            DA.SetDataList(3, out_d);
         }
 
         /// <summary>
@@ -368,13 +370,188 @@ namespace Solver
                     v = v + (1.0 / 2.0) * (a + an) * dt;
                     x = x + vn * dt + beta * (a + 2.0 * an) * (dt * dt);
                 }
-                // 結果を出力マトリクスに入れる。
+                // 結果を出力配列に入れる。
                 out_a[n] = a;
                 out_v[n] = v;
                 out_d[n] = x;
 
                 an = a;
                 vn = v;
+            }
+        }
+    }
+}
+
+namespace ModelView
+{
+    public class ResultViewComponent : GH_Component
+    {
+        public ResultViewComponent()
+            : base("Result View",                           // 名称
+                   "Result View",                           // 略称
+                   "Response Analysis of the Single dof",   // コンポーネントの説明
+                   "rgkr",                                  // カテゴリ(タブの表示名)
+                   "Result"                                 // サブカテゴリ(タブ内の表示名)
+                  )
+        {
+        }
+
+        /// <summary>
+        /// インプットパラメータの登録
+        /// </summary>
+        protected override void RegisterInputParams(GH_InputParamManager pManager)
+        {
+            pManager.AddNumberParameter("Model", "Model", "Model Data", GH_ParamAccess.list);
+            pManager.AddNumberParameter("Result", "Result", "Analysis Result", GH_ParamAccess.list);
+            pManager.AddIntegerParameter("Output Number", "N", "Output Result Number", GH_ParamAccess.item, 0);
+            pManager.AddNumberParameter("Scale", "Sc", "Scale Model", GH_ParamAccess.item, 10);
+            pManager.AddNumberParameter("High", "H", "High Model", GH_ParamAccess.item, 10);
+        }
+
+        /// <summary>
+        /// アウトプットパラメータの登録
+        /// </summary>
+        protected override void RegisterOutputParams(GH_OutputParamManager pManager)
+        {
+            pManager.AddNumberParameter("Surface", "Srf", "Output Model Surface", GH_ParamAccess.item);
+        }
+
+        /// <summary>
+        /// 解析を実行する箇所
+        /// </summary>
+        protected override void SolveInstance(IGH_DataAccess DA)
+        {
+            // パラメータの定義 ＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝
+            List<double> Model = new List<double>();
+            List<double> Rslt = new List<double>();
+            int N = 0;
+            double Sc = double.NaN;
+            double H = double.NaN;
+            double M = double.NaN;
+
+            // grasshopper からデータ取得　＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝
+            if (!DA.GetDataList(0, Model)) { return; }
+            if (!DA.GetDataList(1, Rslt)) { return; }
+            if (!DA.GetData(2, ref N)) { return; }
+
+            // モデルのrhino上への出力＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝
+            Point3d FMorigin = new Point3d(0, 0, H);
+            Point3d FMp1 = new Point3d(1, 0, H);
+            Point3d FMp2 = new Point3d(0, 1, H);
+            Plane FMplane = new Plane(FMorigin, FMp1, FMp2);
+            M = Model[0];
+
+            DA.SetData(0, new Sphere(FMplane, Sc*M));
+        }
+
+        /// <summary>
+        /// GUIDの設定
+        /// </summary>
+        public override Guid ComponentGuid
+        {
+            get { return new Guid("419c3a3a-cc48-4715-9cef-5f5647a5ecfc"); }
+        }
+
+        /// <summary>
+        /// アイコンの設定。24x24 pixelsが推奨
+        /// </summary>
+        protected override System.Drawing.Bitmap Icon
+        {
+            get
+            {
+                return _1dofResponseAnalysis.Properties.Resource.icon;
+            }
+        }
+    }
+
+    public class ModelViewComponent : GH_Component
+    {
+        public ModelViewComponent()
+            : base("Model View",                           // 名称
+                   "Model View",                           // 略称
+                   "Response Analysis of the Single dof",   // コンポーネントの説明
+                   "rgkr",                                  // カテゴリ(タブの表示名)
+                   "Result"                                 // サブカテゴリ(タブ内の表示名)
+                  )
+        {
+        }
+
+        /// <summary>
+        /// インプットパラメータの登録
+        /// </summary>
+        protected override void RegisterInputParams(GH_InputParamManager pManager)
+        {
+            pManager.AddNumberParameter("Model", "Model", "Model Data", GH_ParamAccess.item,100);
+            pManager.AddNumberParameter("Scale", "Sc", "Scale Model", GH_ParamAccess.item, 10);
+            pManager.AddNumberParameter("High", "H", "High Model", GH_ParamAccess.item, 3500);
+        }
+
+        /// <summary>
+        /// アウトプットパラメータの登録
+        /// </summary>
+        protected override void RegisterOutputParams(GH_OutputParamManager pManager)
+        {
+            pManager.AddSurfaceParameter("Surface", "Srf", "Output Model Surface", GH_ParamAccess.item);
+        }
+
+        /// <summary>
+        /// 解析を実行する箇所
+        /// </summary>
+        protected override void SolveInstance(IGH_DataAccess DA)
+        {
+            // パラメータの定義 ＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝
+            double Sc = double.NaN;
+            double H = double.NaN;
+            double M = double.NaN;
+            double K = double.NaN;
+            double Model = double.NaN;
+
+            // grasshopper からデータ取得　＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝
+            if (!DA.GetData(0, ref Model)) { return; }
+            if (!DA.GetData(1, ref Sc)) { return; }
+            if (!DA.GetData(2, ref H)) { return; }
+
+            // 質点の作成＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝
+            Point3d FMorigin = new Point3d(0, 0, H);
+            Point3d FMp1 = new Point3d(1, 0, H);
+            Point3d FMp2 = new Point3d(0, 1, H);
+            Plane FMplane = new Plane(FMorigin, FMp1, FMp2);
+            M = Model;
+            Sphere FirstMass = new Sphere(FMplane, Sc * M);
+
+            // バネの作成＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝
+            Point3d FSPGorigin = new Point3d(0, 0, 0);
+            Point3d FSPGp1 = new Point3d(1, 0, 0);
+            Point3d FSPGp2 = new Point3d(0, 1, 0);
+            Plane FSPGplane = new Plane(FSPGorigin, FSPGp1, FSPGp2);
+            K = Model/10.0;
+            Cylinder FirstSpring = new Cylinder(new Circle(FSPGplane, Sc * K), H-Sc*M);
+
+            // モデルのrhino上への出力＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝
+            var Srf = new Surface[2];
+            Srf[0] = FirstSpring.ToRevSurface();
+            Srf[1] = FirstMass.ToRevSurface();
+
+            DA.SetDataList(0, Srf);
+
+        }
+
+        /// <summary>
+        /// GUIDの設定
+        /// </summary>
+        public override Guid ComponentGuid
+        {
+            get { return new Guid("419c3a3a-cc48-4715-9cef-5f5648a5ecfc"); }
+        }
+
+        /// <summary>
+        /// アイコンの設定。24x24 pixelsが推奨
+        /// </summary>
+        protected override System.Drawing.Bitmap Icon
+        {
+            get
+            {
+                return _1dofResponseAnalysis.Properties.Resource.icon;
             }
         }
     }
